9fa944d41a2392ae5ba36fdfca68a5234a4898cb
Eduarda Teste <poteitounft@gmail.com>
2023-12-27 09:48:53 -0300

    Alterando o método para atualizar a configuração de transferência

--- scripts/modify_scheduled_query.py
+++ scripts/modify_scheduled_query.py
@@ -1,4 +1,4 @@
-from google.cloud import bigquery_datatransfer
+from google.cloud import bigquery_datatransfer_v1 as bqdts
 import google.auth
 import os
 
@@ -6,25 +6,40 @@ import os
 project_id = os.getenv('GCP_PROJECT_ID')
 credentials, _ = google.auth.default()
 
-client = bigquery_datatransfer.DataTransferServiceClient(credentials=credentials)
+client = bqdts.DataTransferServiceClient(credentials=credentials)
 
 # Use o ID do projeto correto ao listar as configurações de transferência
 parent = f"projects/{project_id}"
 
 # Carregue a consulta SQL a partir de um arquivo
-with open(os.getenv("QUERY_FILE"), "r") as sql_file:
+sql_file_path = os.getenv("QUERY_FILE")
+if sql_file_path is None:
+    raise EnvironmentError("The 'QUERY_FILE' environment variable is not set.")
+
+with open(sql_file_path, "r") as sql_file:
     sql = sql_file.read()
 
 # Identificador da consulta agendada
 scheduled_query_name = os.getenv("SCHEDULED_QUERY_NAME")
+if scheduled_query_name is None:
+    raise EnvironmentError("The 'SCHEDULED_QUERY_NAME' environment variable is not set.")
 
 # Encontre a consulta agendada pelo nome
 transfer_configs = client.list_transfer_configs(parent=parent)
 for transfer_config in transfer_configs:
     if transfer_config.display_name == scheduled_query_name:
+        # Prepare the update mask and the new transfer config object
+        update_mask = bqdts.field_mask_pb2.FieldMask(paths=["params"])
+        transfer_config.params["query"] = sql  # Set the SQL query in params
+        
+        # Create the UpdateTransferConfigRequest
+        update_request = bqdts.UpdateTransferConfigRequest(
+            transfer_config=transfer_config,
+            update_mask=update_mask
+        )
+        
         # Atualize a consulta agendada
-        transfer_config.query = sql
-        updated_config = client.update_transfer_config(transfer_config, fields=["query"])
+        updated_config = client.update_transfer_config(update_request)
         print(f"Updated scheduled query: {updated_config.display_name}")
         break
 else:
