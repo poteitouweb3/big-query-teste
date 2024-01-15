7c4e66b0ed5d70b34e1cae7e677c7907ec2c6089
poteitouweb3gamer <122838576+poteitouweb3@users.noreply.github.com>
2024-01-15 14:54:05 -0300

    Adicionando 2 reais precos-produtos.sql

--- queries/precos-produtos.sql
+++ queries/precos-produtos.sql
@@ -1,2 +1,2 @@
-SELECT CODIGO_DO_PRODUTO, SUM(PRECO) AS PRECO, SUM(QUANTIDADE) AS QTD FROM `curso-big-query-9110.sucos_vendas.itens_notas_fiscais`
+SELECT CODIGO_DO_PRODUTO, SUM(PRECO)+2 AS PRECO, SUM(QUANTIDADE) AS QTD FROM `curso-big-query-9110.sucos_vendas.itens_notas_fiscais`
 GROUP BY 1
